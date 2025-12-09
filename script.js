e24f519dc8c0f9b90a093422529702cba994594d
BJ53-droid <josipovicboris7@gmail.com>
2025-12-09 08:21:32 +0100

    bez js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-        var myCarousel = document.getElementById('carouselExampleIndicators');
-        new bootstrap.Carousel(myCarousel, {
-            interval: 5000 // Automatski pokreće karusel
-        });
-    });
